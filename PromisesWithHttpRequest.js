3b7e2e1c34a01d74dfe21ee4405eae40c4ebff26
Souparno Bandyopadhyay <SBandyopadhyay@ingrooves.com>
2018-10-01 11:54:52 -0700

    Working Code to Practice HTTP requests and Promises.
    Code sends a request and prints the HTTP status code received.

---
+++ PromisesWithHttpRequest.js
@@ -0,0 +1,19 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function get(url) {
+    return new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = () => req.status === 200 ? resolve(req.status) : reject(Error(req.status));
+      req.onerror = (e) => reject(Error(`Network Error: ${e}`));
+      req.send();
+    });
+  }
+
+  get('http://www.example.com')
+  .then((reason) => {
+      console.log('got a response ' + reason);
+  })
+  .catch((reason) => {
+      console.log('did not get any responses. ' + reason)
+  });
